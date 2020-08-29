ab8aada1eb9a48f3338787895ff2303c243e75f0
Xiduo <iris5202@gmail.com>
2020-08-30 02:59:26 +0800

    teacher version

---
+++ password_teacher.py
@@ -0,0 +1,10 @@
+password = 'a123456'
+i = 3 #剩餘機會
+while i > 0:
+    pwd = input('輸入密碼: ')
+    if pwd == password:
+        print('登入成功! ')
+        break 
+    else:
+        i = i - 1       
+        print('密碼錯誤!還有',i,'次機會!')
